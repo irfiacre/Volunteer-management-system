5cce2bc3ab695dbae7415d2f2386a3d53d8e9de8
Think <Think@DESKTOP-4N0TEEN>
2023-04-23 14:36:34 +0200

    Add project files.

---
+++ Volunteer management system/Data/AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volunteer_management_system.Models;
+
+namespace Volunteer_management_system.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void onModelCreating(ModelBuilder modelBuilder)
+        //{
+          //  base.OnModelCreating(modelBuilder);
+       // }
+       public DbSet<Users> Users { get; set; }
+        public DbSet<Opportunities> Opportunities { get; set; }
+        public DbSet<Applications> Applications { get; set; }
+
+
+    }
+}
