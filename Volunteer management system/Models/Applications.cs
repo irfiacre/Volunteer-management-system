8646a4dfbf16a59429ce6dd9ad9dbd0083a623d1
Kabera192 <ckabera6@gmail.com>
2023-04-26 10:54:56 +0200

    Changes to the Application model

--- Volunteer management system/Models/Applications.cs
+++ Volunteer management system/Models/Applications.cs
@@ -7,10 +7,20 @@ namespace Volunteer_management_system.Models
     { 
         [Key]
         public int ID { get; set; }
+
+        [Required]
         public string ApplicantFullName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string ApplicantEmail { get; set; }
-        public string OpportunityID { get; set; }
+
         [ForeignKey("OpportunityID")]
+        public string OpportunityID { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 100)]
         public string Note { get; set; }
         public string Status { get; set; }
         public DateTime CreateDate { get; set; }
